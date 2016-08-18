45e4c001b14920b4348cb78e032a887200034d58
Darren Thomas Clark <darren.clark@live.com>
2016-08-17 21:33:27 -0400

    html css complete. stinkin JS is a headache.

---
+++ paperrockscissors.js
@@ -0,0 +1,64 @@
+let userChoice = ''
+let computerChoice = ''
+
+const initialize = () => {
+  document.querySelector('.output').textContent = 'Choose Paper, Rock or Scissors'
+}
+
+document.addEventListener('DOMContentLoaded', initialize)
+
+let rock = document.querySelector('.input button.rock')
+let scissors = document.querySelector('.input button.scissors')
+let paper = document.querySelector('.input button.paper')
+
+let startGame = () => {
+  computerChoice = Math.random()
+  if (computerChoice < 0.33) {
+    computerChoice = 'rock'
+    console.log('rock')
+  } else if (computerChoice <= 0.64) {
+    computerChoice = 'paper'
+    console.log('paper')
+  } else {
+    computerChoice = 'scissors'
+    console.log('scissors')
+  }
+  document.querySelector('h1').textContent = 'I chose...' + computerChoice
+  compare(userChoice, computerChoice)
+}
+let compare = (choice1, choice2) => {
+  if (choice1 === choice2) {
+    document.querySelector('.output').textContent = 'A Draw!'
+  } if (choice1 === 'scissors') {
+    if (choice2 === 'paper') {
+      document.querySelector('.output').textContent = 'You Win!'
+    } else {
+      document.querySelector('.output').textContent = 'You Lose!'
+    }
+  } else if (choice1 === 'paper') {
+    if (choice2 === 'rock') {
+      document.querySelector('.output').textContent = 'You Win!'
+    } else {
+      document.querySelector('.output').textContent = 'You Lose!'
+    }
+  } else if (choice1 === 'rock') {
+    if (choice2 === 'scissors') {
+      document.querySelector('.output').textContent = 'You Win!'
+    } else {
+      document.querySelector('.output').textContent = 'You Lose!'
+    }
+  }
+}
+
+rock.addEventListener('click', function () {
+  userChoice = 'rock'
+  startGame()
+})
+scissors.addEventListener('click', function () {
+  userChoice = 'scissors'
+  startGame()
+})
+paper.addEventListener('click', function () {
+  userChoice = 'paper'
+  startGame()
+})
