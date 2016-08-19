3b07932ff51b3d572d7598fa027132a09dadb1f6
Darren Thomas Clark <darren.clark@live.com>
2016-08-18 22:55:26 -0400

    Final. UI and JS working.

--- paperrockscissors.js
+++ paperrockscissors.js
@@ -4,68 +4,71 @@ let computerMove = ''
 // TEXT INSTRUCTIONS
 const initialize = () => {
   document.querySelector('.output1').textContent = 'Choose Paper, Rock or Scissors'
+
+  let rock = document.querySelector('.rock')
+  let scissors = document.querySelector('.scissors')
+  let paper = document.querySelector('.paper')
+
+  // FOR LOOP CONTAINING MATH LOGIC
+  let startGame = () => {
+    computerMove = Math.random()
+    if (computerMove < 0.33) {
+        computerMove = "Rock"
+    } else if (computerMove <= 0.64) {
+      computerMove = 'Paper'
+    } else {
+      computerMove = 'Scissors'
+    }
+  // I DONT WANT THIS TEXT AREA---JUST WIN, LOSE OR DRAW
+    document.querySelector('.computerreturn').textContent = '{I chose... ' + computerMove + "}"
+    compare(yourMove, computerMove)
+  }
+  // VARIABLE COMPARISONS
+
+  let compare = (choice1, choice2) => {
+    console.log("your move: ", choice1, 'comp move: ', choice2);
+    if (choice1 == choice2) {
+      console.log('working');
+      document.querySelector('.textstatus').textContent = 'A Draw!'
+    }else if (choice1 === 'Scissors') {
+      if (choice2 === 'Paper') {
+        document.querySelector('.textstatus').textContent = 'You Win!'
+      } else {
+        document.querySelector('.textstatus').textContent = 'You Lose!'
+      }
+    }else if (choice1 === 'Paper') {
+      if (choice2 === 'Rock') {
+        // var node = document.createElement("img")  //<img/>
+        // node.setAttribute('src','./images/rocksmall.png') //<img src='./images/rocksmall.png' />
+        // const parentDiv = document.querySelector('.statuscontainerinternal')
+        // const childDiv = document.querySelector('.textstatus')
+        // parentDiv.insertBefore(node, childDiv)
+        document.querySelector('.textstatus').textContent = 'You Win!'
+      } else {
+        document.querySelector('.textstatus').textContent = 'You Lose!'
+      }
+    }else if (choice1 === 'Rock') {
+      if (choice2 === 'Scissors') {
+        document.querySelector('.textstatus').textContent = 'You Win!'
+      } else {
+        document.querySelector('.textstatus').textContent = 'You Lose!'
+      }
+    }
+  }
+
+  // CLICK FUNCTIONALITY
+  rock.addEventListener('click', function () {
+    yourMove = 'Rock'
+    startGame()
+  })
+  scissors.addEventListener('click', function () {
+    yourMove = 'Scissors'
+    startGame()
+  })
+  paper.addEventListener('click', function () {
+    yourMove = 'Paper'
+    startGame()
+  })
 }
 
 document.addEventListener('DOMContentLoaded', initialize)
-
-let rock = document.querySelector('.rock')
-let scissors = document.querySelector('.scissors')
-let paper = document.querySelector('.paper')
-
-// FOR LOOP CONTAINING MATH LOGIC
-let startGame = () => {
-  computerMove = Math.random()
-  if (computerMove < 0.33) {
-    computerMove = 'rock'
-    console.log('rock')
-  } else if (computerMove <= 0.64) {
-    computerMove = 'paper'
-    console.log('paper')
-  } else {
-    computerMove = 'scissors'
-    console.log('scissors')
-  }
-
-// I DONT WANT THIS TEXT AREA---JUST WIN, LOSE OR DRAW
-  document.querySelector('textstatus').textContent = 'I chose...' + computerMove
-  compare(yourMove, computerMove)
-
-// VARIABLE COMPARISONS
-}
-let compare = (choice1, choice2) => {
-  if (choice1 === choice2) {
-    document.querySelector('.textstatus').textContent = 'A Draw!'
-  } if (choice1 === 'scissors') {
-    if (choice2 === 'paper') {
-      document.querySelector('.output').textContent = 'You Win!'
-    } else {
-      document.querySelector('.output').textContent = 'You Lose!'
-    }
-  } else if (choice1 === 'paper') {
-    if (choice2 === 'rock') {
-      document.querySelector('.output').textContent = 'You Win!'
-    } else {
-      document.querySelector('.output').textContent = 'You Lose!'
-    }
-  } else if (choice1 === 'rock') {
-    if (choice2 === 'scissors') {
-      document.querySelector('.output').textContent = 'You Win!'
-    } else {
-      document.querySelector('.output').textContent = 'You Lose!'
-    }
-  }
-}
-
-// CLICK FUNCTIONALITY
-rock.addEventListener('click', function () {
-  yourMove = 'rock'
-  startGame()
-})
-scissors.addEventListener('click', function () {
-  yourMove = 'scissors'
-  startGame()
-})
-paper.addEventListener('click', function () {
-  yourMove = 'paper'
-  startGame()
-})
